cdaa90ea355672b8305e682dce2b69512faee8d0
Egsagon <83862309+Egsagon@users.noreply.github.com>
2023-04-26 13:51:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ent, json
 
 client = ent.ENT(**json.load(open('creds.json')))
 
-rec = client.userbase.search_users('KEIZER')[0]
+rec = client.userbase.search_users()[0]
 
 print(
     client.rack.deposit(rec, 'test.txt')
